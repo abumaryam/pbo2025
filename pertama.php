938a28c2f93d3888bc8ec2086df31e813f94eb00
abumaryam <wiradianxiv@mti.gadjahmada.edu>
2025-08-22 15:53:11 +0700

    Add detailed content to pertama.php with placeholder text

--- pertama.php
+++ pertama.php
@@ -1,3 +1,8 @@
 <?php
 
 echo "Hello";
+echo "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, urna eu tincidunt consectetur, nisi nisl aliquam nunc, eget aliquam massa nisl quis neque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>";
+echo "<p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer ac sem nec urna cursus faucibus. Etiam euismod, justo at facilisis cursus, urna erat dictum velit, nec cursus erat urna a erat.</p>";
+echo "<p>Morbi non urna vitae elit cursus dictum. Suspendisse potenti. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi.</p>";
+echo "<p>Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.</p>";
+echo "<p>Phasellus molestie magna non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus. Mauris iaculis porttitor posuere. Praesent id metus massa, ut blandit odio.</p>";
