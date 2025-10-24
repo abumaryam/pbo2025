f04d769b90de48127caf1551ab3ac9f40faa8f4b
abumaryam <wiradianxiv@mti.gadjahmada.edu>
2025-10-24 14:20:10 +0700

    Praktikum 9A - Modifikasi Class Dosen

--- app/Akademik/Dosen.php
+++ app/Akademik/Dosen.php
@@ -1,14 +1,18 @@
 <?php
 
+
 namespace App\Akademik;
+
 
 use App\Akademik\Pegawai;
 use App\Akademik\PenilaianKinerja;
+
 
 class Dosen extends Pegawai implements PenilaianKinerja
 {
     private int $nidn;
     public int $jumlah_sks;
+
 
     public function __construct(int $nip, string $nama, string $no_hp, string $alamat, string $nidn)
     {
@@ -17,15 +21,18 @@ class Dosen extends Pegawai implements PenilaianKinerja
         $this->jumlah_sks = 24;
     }
 
+
     public function bekerja(): void
     {
         echo $this->nama . " sedang mengajar dan membimbing mahasiswa.<br>";
     }
 
+
     public function hitungTunjanganKinerja(): int
     {
         return $this->jumlah_sks * 150000;
     }
+
 
     public function getNidn(): int
     {
